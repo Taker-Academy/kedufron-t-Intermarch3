aed79283901da68858baa946b5b26566b83198e6
Intermarch3 <lucasleclerc74@gmail.com>
2024-02-20 23:44:26 +0100

    [ADD] add v1 of saving article to cart

--- js/article.js
+++ js/article.js
@@ -2,12 +2,14 @@
 const path = 'https://api.kedufront.juniortaker.com/'
 const imgPath = 'https://api.kedufront.juniortaker.com/item/picture/'
 
-// recuperation de l'element container
+// recuperation des elements a modifier de l'article
 let imgContainer = document.getElementById('img');
 let nameContainer = document.getElementsByClassName('nom')[0];
 let DescriptionContainer = document.getElementsByClassName('description')[0];
 let fromContainer = document.getElementsByClassName('from')[0];
 let PriceContainer = document.getElementsByClassName('prix')[0];
+let btnPrice = document.getElementsByClassName('button')[0].attributes[0];
+let btn = document.getElementById('buy-btn');
 
 const id = window.location.search.substring(4);
 
@@ -18,7 +20,11 @@ axios.get(path + `item/${id}`)
         DescriptionContainer.innerText = response.data.item.description
         fromContainer.innerText = response.data.item.createdIn
         PriceContainer.innerText = response.data.item.price + '€'
+        btnPrice.value = 'Price: ' + response.data.item.price + '€'
         imgContainer.style.backgroundImage = `url('${imgPath + id}')`
+        btn.addEventListener('click', () => {
+            addToCart();
+        });
     })
     .catch(error => {
         console.log(error);
@@ -27,9 +33,17 @@ axios.get(path + `item/${id}`)
 
 // gestion ajout au panier
 function addToCart() {
-    if (localStorage.getItem(string(id))) {
-        localStorage.setItem(string(id), parseInt(localStorage.getItem(string(id))) + 1)
+    const strId = id.toString();
+    let cart = localStorage.getItem('cart');
+    if (cart === null) {
+        cart = {};
     } else {
-        localStorage.setItem(string(id), 1)
+        cart = JSON.parse(cart);
     }
+    try {
+        cart[strId] += 1;
+    } catch (error) {
+        cart[strId] = 1;
+    }
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
