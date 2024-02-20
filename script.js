a8b5ea40f32de84d8df05a1c8a00a3668bf3532e
Intermarch3 <lucasleclerc74@gmail.com>
2024-02-20 19:42:46 +0100

    [ADD] add comments

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// Constantes
 const path = 'https://api.kedufront.juniortaker.com/'
 const imgPath = 'https://api.kedufront.juniortaker.com/item/picture/'
 
+// recuperation de l'element container
 const articlesContainer = document.getElementById('container');
 
+// Requete pour recuperer les articles
 axios.get(path + 'item/')
     .then(function(response) {
         response.data.forEach(article => {
@@ -12,6 +15,7 @@ axios.get(path + 'item/')
             <div style="background-image: url('${imgPath + article._id}')" alt="${article.name}">
             <h2>${article.name}</h2>
             <p>Prix : ${article.price}€</p>
+            <button>Acheter</button>
             `;
             articlesContainer.appendChild(card);
         })
