61cf6116d74903cccd6e61fd378ca45d4737f0af
Intermarch3 <lucasleclerc74@gmail.com>
2024-02-21 11:30:24 +0100

    [ADD] add btn to change number of articles in cart

--- js/panier.js
+++ js/panier.js
@@ -12,19 +12,40 @@ Object.keys(cart).forEach((id) => {
             let elem = response.data.item;
             let article = document.createElement('div');
             article.className = 'article';
+            article.id = id;
             article.innerHTML = `
             <div class="img" style="background-image: url('${imgPath + id}')" alt="${elem.name}"></div>
             <h2>${elem.name}</h2>
             <p>Prix : ${elem.price}€</p>
             <div class="article-btns">
                 <button class="article-del">-</button>
-                <p>${cart[id]}</p>
+                <p class="nb-elem">${cart[id]}</p>
                 <button class="article-add">+</button>
             </div>
                 `;
             cartContainer.appendChild(article);
+            article.addEventListener('click', (e) => {
+                if (e.target.classList.contains('article-del')) {
+                    changeCart(id, -1);
+                } else if (e.target.classList.contains('article-add')) {
+                    changeCart(id, 1);
+                }
+            })
         })
         .catch(error => {
             console.log(error);
         });
 });
+
+// changer la quantité d'un article
+function changeCart(id, value) {
+    let cart = JSON.parse(localStorage.getItem('cart'));
+    if (cart[id] + value >= 1) {
+        cart[id] += value;
+        document.getElementById(id.toString()).children[3].children[1].innerText = cart[id].toString();
+    } else {
+        delete cart[id];
+        document.getElementById(id).remove();
+    }
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
