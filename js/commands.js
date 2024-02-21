a52d8ccb5417e90c20943957d2921eff3ebcd622
Intermarch3 <lucasleclerc74@gmail.com>
2024-02-21 18:26:00 +0100

    [ADD] add command page and display last commands

---
+++ js/commands.js
@@ -0,0 +1,22 @@
+// elements
+let commands = JSON.parse(localStorage.getItem('commands'));
+let commandsContainer = document.getElementById('commands');
+
+// afficher les elements du panier
+commands.forEach((command) => {
+    let commandElem = document.createElement('div');
+    commandElem.className = 'command-elem';
+    commandElem.innerHTML = `
+        <h3>Commande ID: ${command.command_id}</h3>
+        <p>status: ${command.ok == true ? 'ok':'erreur'}</p>
+    `;
+    commandsContainer.appendChild(commandElem);
+});
+
+// verifier si le panier est vide
+function checkComands() {
+    let commands = localStorage.getItem('cart');
+    if (commands === null || Object.keys(JSON.parse(commands)).length === 0) {
+        commandsContainer.innerHTML = '<h2>Votre panier est vide</h2>';
+    }
+}
